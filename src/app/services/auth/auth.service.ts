60a7bebb78be4d7af74f7b49ed319cabc2de2d24
attackq <rastpk@gmail.com>
2022-04-29 22:51:59 +0300

    addUser modified

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import firebase from "firebase/compat/app";
+import {collection, query, where} from "firebase/firestore";
 import AuthProvider = firebase.auth.AuthProvider;
-import {filter, from, Observable, ReplaySubject, switchMap, tap} from "rxjs";
+import {filter, from, Observable, of, ReplaySubject, switchMap, tap} from "rxjs";
 import UserCredential = firebase.auth.UserCredential;
-import {user} from "@angular/fire/auth";
 import {Collections} from "../crud/collections";
 import {CrudService} from "../crud/crud.service";
-import {PostStore, User, UserStore} from "../../post";
+import {User, UserStore} from "../../post";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -24,16 +25,44 @@ export class AuthService {
       tap((value: firebase.User | null) => this.user$.next(value)),
       filter((value: firebase.User | null) => !!value),
       switchMap((value: firebase.User | null) => {
-        const user: User = {
-          email: value?.email!,
-          name: value?.displayName!,
-          img: value?.photoURL!,
-          id: value?.uid!
-        }
-        return this.crudService.createObject(Collections.USERS, user);
-      })).subscribe();
+        return this.userStore.pipe(
+          map((users: UserStore[]) => {
+            users.forEach((user: UserStore) => {
+              if (user.email !== value?.email) {
+                const newUser: User = {
+                  email: value?.email!,
+                  name: value?.displayName!,
+                  img: '111111111',
+                  id: value?.uid!
+                }
+                console.log(newUser);
+                return newUser
+              } else {
+                return null;
+              }
+            })
+          }),
+          switchMap(value => this.crudService.createObject(Collections.USERS, value)));
+      })).subscribe()
   }
 
+
+//   this.afAuth.authState.pipe(
+//     tap((value: firebase.User | null) => this.user$.next(value)),
+//   filter((value: firebase.User | null) => !!value),
+//   switchMap((userFromSLogin: firebase.User | null) => {
+//   return  this.userStore(тут делаешь запрос с кверей, то есть проверяешь есть ли пользователь с таким эмейломм).map((
+//   userFromStore) => if(userFromStore) {
+//     return null;
+//   } else {
+//   Если такой юзер в сторе уже есть, возвращаем нал, так как нам не интересно, если такого юзера нету, то возвращаем юзера с логина
+//   return of(userFromSLogin)
+// }
+// ))
+// }),
+// filter((value: firebase.User | null) => !!value)
+// switchMap((value: firebase.User | null) => this.crudService.createObject(Collections.USERS, user)))
+// ).subscribe();
 
   public googleSingIn(): Observable<UserCredential> {
     return this.authWithPopup(new firebase.auth.GoogleAuthProvider());
